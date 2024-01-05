0a42629a8abc50e5704204ce0387f11356660362
AkashSheri98 <akashsheri2367@gmail.com>
2024-01-05 20:36:58 +0530

    mobile final deploy

--- src/components/notesContentMobile/MobileNotesContent.jsx
+++ src/components/notesContentMobile/MobileNotesContent.jsx
@@ -4,22 +4,22 @@ import styles from "../../components/notesContentMobile/MobileNotesContent.modul
 const MobileNotesContent = ({note}) =>{
     return(
         <div className={styles.bg}>
-        <div className={styles.notifications_container}>
-            <div className={styles.success}>
-                <div className={styles.flex}>                
-                    <div className={styles.success_prompt_wrap}>
-                        <div className={styles.success_prompt_prompt}>
-                            <p> {note.content} </p>
+            <div className={styles.notifications_container}>
+                <div className={styles.success}>
+                    <div className={styles.flex}>                
+                        <div className={styles.success_prompt_wrap}>
+                            <div className={styles.success_prompt_prompt}>
+                                <p>{note.content}</p>
+                            </div>
+                            
+                    
                         </div>
-                        
-                
+                    </div>
+                    <div className={styles.success_button_container}>
+                        <p>{note.date}</p><ul></ul><p>{note.time}</p>
                     </div>
                 </div>
-                <div className={styles.success_button_container}>
-                    <p>{note.date}</p><ul></ul><p>{note.time}</p>
-                </div>
             </div>
-        </div>
         </div>
     );
 };
